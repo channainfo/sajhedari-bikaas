afc55c9819e68a4dc8b227da8f23060d39f639ba
Reaksmey <reaksmey@instedd.org>
2013-09-03 15:32:30 +0700

    add test for conflict case

--- spec/models/conflict_case_spec.rb
+++ spec/models/conflict_case_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require '../spec_helper'
 
 describe ConflictCase do
   describe '#field_description' do
@@ -51,6 +51,84 @@ describe ConflictCase do
   	  	expect{ @conflict.field_description(@fields, 'con_intensity')}.to raise_error(Exception)		
   	  end
   	end
+
+    describe "generate graph" do
+      before(:each) do
+        @params = {:data => "1", :from => "08/01/2013", :to => "09/30/2013"}
+        ConflictCase.create!(:conflict_intensity => 1, :conflict_state => 1, :conflict_type => 1)
+        ConflictCase.create!(:conflict_intensity => 1, :conflict_state => 1, :conflict_type => 1)
+        @conflict_case = ConflictCase.where(:conflict_type => 1)
+      end
+
+      it "should generate array format for display daily report on google chart" do
+        expected_result = [[["8/1/2013", 0], ["8/2/2013", 0], ["8/3/2013", 0], ["8/4/2013", 0], ["8/5/2013", 0], ["8/6/2013", 0],
+                  ["8/7/2013", 0], ["8/8/2013", 0], ["8/9/2013", 0], ["8/10/2013", 0], ["8/11/2013", 0], ["8/12/2013", 0],
+                  ["8/13/2013", 0], ["8/14/2013", 0], ["8/15/2013", 0], ["8/16/2013", 0], ["8/17/2013", 0], ["8/18/2013", 0],
+                  ["8/19/2013", 0], ["8/20/2013", 0], ["8/21/2013", 0], ["8/22/2013", 0], ["8/23/2013", 0], ["8/24/2013", 0],
+                  ["8/25/2013", 0], ["8/26/2013", 0], ["8/27/2013", 0], ["8/28/2013", 0], ["8/29/2013", 0], ["8/30/2013", 0], ["8/31/2013", 0],
+                  ["9/1/2013", 0], ["9/2/2013", 0], ["9/3/2013", 2], ["9/4/2013", 0], ["9/5/2013", 0], ["9/6/2013", 0],
+                  ["9/7/2013", 0], ["9/8/2013", 0], ["9/9/2013", 0], ["9/10/2013", 0], ["9/11/2013", 0], ["9/12/2013", 0],
+                  ["9/13/2013", 0], ["9/14/2013", 0], ["9/15/2013", 0], ["9/16/2013", 0], ["9/17/2013", 0], ["9/18/2013", 0],
+                  ["9/19/2013", 0], ["9/20/2013", 0], ["9/21/2013", 0], ["9/22/2013", 0], ["9/23/2013", 0], ["9/24/2013", 0],
+                  ["9/25/2013", 0], ["9/26/2013", 0], ["9/27/2013", 0], ["9/28/2013", 0], ["9/29/2013", 0], ["9/30/2013", 0]], ["6"]]
+
+        ConflictCase.generate_daily_graph(@conflict_case, @params).should eq expected_result
+      end
+
+      it "should generate array format for display weekly report on google chart" do
+        expected_result = [[["W1/8/2013", 0], ["W2/8/2013", 0], ["W3/8/2013", 0], ["W4/8/2013", 0],
+                          ["W1/9/2013", 2], ["W2/9/2013", 0], ["W3/9/2013", 0], ["W4/9/2013", 0]],["6"]]
+
+        ConflictCase.generate_weekly_graph(@conflict_case, @params).should eq expected_result
+      end
+
+      it "should generate array format for disply montly report on google chart" do
+        expected_result = [[["8/2013", 0], ["9/2013", 2]], ["6"]]
+        ConflictCase.generate_montly_graph(@conflict_case, @params).should eq expected_result
+      end
+
+      it "should generate array format for display quarterly report on google chart" do
+        expected_result = [[["Q3/2013", 2]], ["6"]]
+        ConflictCase.generate_quarterly_graph(@conflict_case, @params).should eq expected_result
+      end
+
+      it "should generate array format for display semi-annualy report on google chart" do
+        expected_result = [[["S2/2013", 2]], ["6"]]
+        ConflictCase.generate_semi_annual_graph(@conflict_case, @params).should eq expected_result
+      end
+
+      it "should generate array format for display yearly report on google chart" do
+        expected_result = [[["2013", 2]], ["6"]]
+        ConflictCase.generate_yearly_graph(@conflict_case, @params).should eq expected_result
+      end
+    end
+
+    describe "manualy set the max high of google chart" do
+
+      it "should return 6 when the array is empty" do
+        array_value = []
+        max_value = ConflictCase.calculate_max_vAxis(array_value)
+        max_value.should eq 6
+      end
+
+      it "should return 6 when the max value in array less than 6" do
+        array_value = [1, 2, 3, 4]
+        max_value = ConflictCase.calculate_max_vAxis(array_value)
+        max_value.should eq 6
+      end
+
+      it "should return the max value if it is devided by 6" do
+        array_value = [1, 2, 3, 4, 5, 6]
+        max_value = ConflictCase.calculate_max_vAxis(array_value)
+        max_value.should eq 6
+      end
+
+      it "should return the value division of 6 if it is not devided by 6" do
+        array_value = [1, 2, 3, 4, 5, 6, 7]
+        max_value = ConflictCase.calculate_max_vAxis(array_value)
+        max_value.should eq 12
+      end
+    end
 
   end
 end
