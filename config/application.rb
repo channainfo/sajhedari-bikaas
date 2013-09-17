64085357327c6008038b0ac8e0d6a90f503ff8dc
sokmesa <sokmesa@sokmesa-ThinkPad-T410.(none)>
2013-09-17 14:35:16 +0700

    fix date time to use Kathmandu

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Conflict
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.time_zone = 'UTC'
-    Time.zone = "Bangkok"
+    Time.zone = "Kathmandu"
   end
 end
